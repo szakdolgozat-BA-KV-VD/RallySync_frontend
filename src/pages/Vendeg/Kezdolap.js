046a3fe0511ca84b3e245412f50680678a1c0451
BeldyAdam <beldy.adam@gmail.com>
2025-03-06 16:30:15 +0100

    kezdolap szerk.

--- src/pages/Vendeg/Kezdolap.js
+++ src/pages/Vendeg/Kezdolap.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import useAuthContext from '../../contexts/AuthContext';
 
-export default function Kezdolap() {
-     const { user } = useAuthContext(); 
-    return (
-        <div>
-            <h1>Kezdőlap</h1>
-            <p>Bejelentkezett felhasználó: { user===null?"Nincs bejelentkezett felhasználó!":user.name }</p>
-        </div>
-    );
+function Kezdolap() {
+  const { user } = useAuthContext();
+  return (
+    <div>
+      <h1>Kezdőlap</h1>
+      <p>Bejelentkezett felhasználó: {user === null ? "Nincs bejelentkezett felhasználó!" : user.name}</p>
+    </div>
+  );
 }
+
+
+export default Kezdolap;
